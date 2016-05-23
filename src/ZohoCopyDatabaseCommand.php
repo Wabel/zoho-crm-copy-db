57f1aa4a284b0f6a114c10329331f146fc843fc2
Raphaël BERGINA <raphael.bergina@wabel.com>
2016-05-23 17:12:34 +0200

    add sync command in ZohoCopyDatabaseCommand

--- src/ZohoCopyDatabaseCommand.php
+++ src/ZohoCopyDatabaseCommand.php
@@ -9,7 +9,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Wabel\Zoho\CRM\AbstractZohoDao;
 
 class ZohoCopyDatabaseCommand extends Command
@@ -27,37 +28,67 @@ class ZohoCopyDatabaseCommand extends Command
     private $zohoDatabaseCopier;
 
     /**
+     * @var ZohoDatabaseSyncZoho
+     */
+    private $zohoDatabaseSync;
+
+    /**
      * @var Lock
      */
-    private $lock;
+    private $lockCopy;
+    
+    /**
+     * @var Lock
+     */
+    private $lockSync;
 
     /**
      * @param ZohoDatabaseCopier                $zohoDatabaseCopier
      * @param \Wabel\Zoho\CRM\AbstractZohoDao[] $zohoDaos           The list of Zoho DAOs to copy
      * @param Lock                              $lock               A lock that can be used to avoid running the same command twice at the same time
      */
-    public function __construct(ZohoDatabaseCopier $zohoDatabaseCopier, array $zohoDaos, Lock $lock = null)
+    public function __construct(ZohoDatabaseCopier $zohoDatabaseCopier, ZohoDatabaseSyncZoho $zohoDatabaseSync, array $zohoDaos, Lock $lockCopy = null, Lock $lockSync = null)
     {
         parent::__construct();
         $this->zohoDatabaseCopier = $zohoDatabaseCopier;
+        $this->zohoDatabaseSync = $zohoDatabaseSync;
         $this->zohoDaos = $zohoDaos;
-        $this->lock = $lock;
+        $this->lockCopy = $lockCopy;
+        $this->lockSync = $lockSync;
     }
 
     protected function configure()
     {
         $this
             ->setName('zoho:copy-db')
-            ->setDescription('Copies the Zoho database in local DB tables')
+            ->setDescription('Copies the Zoho database in local DB tables and synchronize Zoho CRM from the Zoho database.')
+            ->addArgument("action",  InputArgument::REQUIRED, "Specify 'copy' or 'sync'")
             ->addOption("reset", "r", InputOption::VALUE_NONE, 'Get a fresh copy of Zoho (rather than doing incremental copy)')
             ->addOption("trigger", "t", InputOption::VALUE_NONE, 'Create or update the triggers');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $action = $input->getArgument('action');
+        switch ($action) {
+            case 'copy':
+                $this->copyDb($input, $output);
+                break;
+            case 'sync':
+                $this->syncDb($input, $output);
+                break;
+            default:
+                throw new InvalidArgumentException('Named argument not found.');
+                break;
+        }
+        
+
+    }
+
+    private function copyDb(InputInterface $input, OutputInterface $output){
         try {
-            if ($this->lock) {
-                $this->lock->acquireLock();
+            if ($this->lockCopy) {
+                $this->lockCopy->acquireLock();
             }
 
             if ($input->getOption('reset')) {
@@ -65,9 +96,9 @@ class ZohoCopyDatabaseCommand extends Command
             } else {
                 $incremental = true;
             }
-            
+
             $forceCreateTrigger = false;
-            
+
             if($input->getOption('trigger')){
                 $forceCreateTrigger = true;
             }
@@ -80,11 +111,36 @@ class ZohoCopyDatabaseCommand extends Command
                 $this->zohoDatabaseCopier->copy($zohoDao, $incremental, $twoWaysSync, $forceCreateTrigger);
             }
             $output->writeln('Zoho data successfully copied.');
-            if ($this->lock) {
-                $this->lock->releaseLock();
+            if ($this->lockCopy) {
+                $this->lockCopy->releaseLock();
             }
         } catch (LockException $e) {
-            $output->writeln('<error>Could not start zoho:copy-db command. Another zoho:copy-db command is already running.</error>');
+            $output->writeln('<error>Could not start zoho:copy-db copy command. Another zoho:copy-db copy command is already running.</error>');
+        }
+    }
+
+    private function syncDb(InputInterface $input, OutputInterface $output){
+        try {
+            if ($this->lockSync) {
+                $this->lockSync->acquireLock();
+            }
+            $this->zohoDatabaseSync->setLogger(new ConsoleLogger($output));
+
+            $output->writeln('Starting synchronize Zoho data into Zoho CRM.');
+            foreach ($this->zohoDaos as $zohoDao) {
+                $output->writeln(sprintf(' > Insert new rows using <info>%s</info>', get_class($zohoDao)));
+                $this->zohoDatabaseSync->pushInsertedRows($zohoDao);
+                $output->writeln(sprintf(' > Update rows using <info>%s</info>', get_class($zohoDao)));
+                $this->zohoDatabaseSync->pushUpdatedRows($zohoDao);
+                $output->writeln(sprintf(' > Delete rows using <info>%s</info>', get_class($zohoDao)));
+                $this->zohoDatabaseSync->pushDeletedRows($zohoDao);
+            }
+            $output->writeln('Zoho data successfully synchronized.');
+            if ($this->lockSync) {
+                $this->lockSync->releaseLock();
+            }
+        } catch (LockException $e) {
+            $output->writeln('<error>Could not start zoho:sync-db sync command. Another zoho:sync-db sync command is already running.</error>');
         }
     }
 }
