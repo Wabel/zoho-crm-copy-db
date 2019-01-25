2cd2187a1de4d27b69f3dca729e0d9c86d2e893b
Raphaël BERGINA <rbergDrox@users.noreply.github.com>
2019-01-25 14:12:38 +0100

    Update ZohoDatabaseCopier.php
    Use modifiedTime instead of lastActivityTime because it changes when the record or related records changed

--- src/ZohoDatabaseCopier.php
+++ src/ZohoDatabaseCopier.php
@@ -125,15 +125,17 @@ class ZohoDatabaseCopier
 
         if ($incrementalSync) {
             $this->logger->info("Copying incremental data for '$tableName'");
-            /////'SHOW COLUMNS FROM '.$tableName.' LIKE `lastActivityTime`');
             // Let's get the last modification date:
             $tableDetail = $this->connection->getSchemaManager()->listTableDetails($tableName);
-            //We use createdTime instead of lastActivityTime when the field doesn't exist in order to follow changes.
-            if($tableDetail->hasColumn('lastActivityTime')){
-                $lastActivityTime = $this->connection->fetchColumn('SELECT MAX(lastActivityTime) FROM '.$tableName);
-            } else{
+            $lastActivityTime = null;
+            if($tableDetail->hasColumn('modifiedTime')){
+                $lastActivityTime = $this->connection->fetchColumn('SELECT MAX(modifiedTime) FROM '.$tableName);
+            }
+            if(!$lastActivityTime && $tableDetail->hasColumn('createdTime'))
+            {
                 $lastActivityTime = $this->connection->fetchColumn('SELECT MAX(createdTime) FROM '.$tableName);
             }
+
             if ($lastActivityTime !== null) {
                 $lastActivityTime = new \DateTime($lastActivityTime);
                 $this->logger->info('Last activity time: '.$lastActivityTime->format('c'));
